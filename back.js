b1135e96294ec621a9e938906bf916ae886a33d1
ortegaha <ortegaha2008.@gmail.com>
2025-05-05 01:54:25 -0600

    Add files via upload

---
+++ back.js
@@ -0,0 +1,26 @@
+function Pg1(){
+    document.getElementById("button1").style.backgroundColor = "red";
+    document.getElementById("button2").style.backgroundColor = "red";
+    document.getElementById("button3").style.backgroundColor = "green";
+}
+
+function Pg2(){
+    document.getElementById("button1").style.backgroundColor = "red";
+    document.getElementById("button2").style.backgroundColor = "green";
+    document.getElementById("button3").style.backgroundColor = "red";
+}
+
+function Pg3(){
+    document.getElementById("button1").style.backgroundColor = "green";
+    document.getElementById("button2").style.backgroundColor = "red";
+    document.getElementById("button3").style.backgroundColor = "red";
+}
+
+
+function Correcto(){
+    document.getElementById("button1").style.backgroundColor = "red";
+    document.getElementById("button2").style.backgroundColor = "red";
+    document.getElementById("button3").style.backgroundColor = "green";
+    var sum;
+    sum += 1
+}
