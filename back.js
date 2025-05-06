b11537dc1110fb42b87d821fec2edd4c3d9d4d4e
ortegaha <ortegaha2008.@gmail.com>
2025-05-06 00:47:53 -0600

    Add files via upload
    primer prototipo de mi quizz

---
+++ back.js
@@ -0,0 +1,24 @@
+
+function Pg1(){
+    document.getElementById("button3").style.backgroundColor = "green";
+    document.getElementById("button1").disabled = true;
+    document.getElementById("button2").disabled = true;
+}
+
+function Pg2(){
+    document.getElementById("button2").style.backgroundColor = "green";
+    document.getElementById("button3").disabled = true;
+    document.getElementById("button1").disabled = true;
+}
+
+
+function Pg3(){
+    document.getElementById("button1").style.backgroundColor = "green";
+    document.getElementById("button3").disabled = true;
+    document.getElementById("button2").disabled = true;
+}
+
+
+
+
+
